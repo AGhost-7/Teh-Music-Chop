0a8bd15526b018d9cc571b1230fc14f5e628bb97
AGhost-7 <hellomynameisjony@gmail.com>
2014-11-19 18:02:11 -0500

    ...

--- about.php
+++ about.php
@@ -9,6 +9,7 @@
 		<?php include 'templates/navbar.php'; ?>
 		<p>Data from <a href="http://stevesmusic.com">Steve's Music Store</a> was scrapped to populate this site.</p>
 		<p>Source can be found on <a href="https://github.com/AGhost-7/Teh-Music-Chop">Github</a>.</p>
+		<p>The demo administration account is hello_world, password is hello_world.</p>
 	</div>
 	</body>
 </html>
